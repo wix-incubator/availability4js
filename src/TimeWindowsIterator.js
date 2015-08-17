b6b0523a961c2054b9b082453a335745472ac58a
3LOK <yoava@wix.com>
2015-08-17 12:19:11 +0300

    Changed default to work always

--- src/TimeWindowsIterator.js
+++ src/TimeWindowsIterator.js
@@ -4,7 +4,7 @@ var Status = require("./Status.js");
 
 module.exports = function(params) {
 	params = params || {};
-	var av = params.availability || null; // availability.Availability
+	var av = params.availability || {}; // availability.Availability
 	var cal = params.cal || null; // Moment with tz
 	
 	var self = {};
