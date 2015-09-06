756ebc6e9bf23bf29b656af46318b9d9a0cdbf97
dleshem <dleshem@gmail.com>
2015-09-06 19:05:03 +0300

    Extracted DateTimeWindows insertion logic to separate class.

--- src/DateTimeWindowsIterator.js
+++ src/DateTimeWindowsIterator.js
@@ -2,26 +2,8 @@
 
 import * as Status from "./Status.js"
 import moment from 'moment-timezone'
+import {Index, DateTimeWindowsInserter} from "./DateTimeWindowsInserter.js"
 
-class Index {
-	/**
-	 * @param index           Integer
-	 * @param isDummyBefore   Boolean
-	 */
-	constructor(index, isDummyBefore) {
-		this.index = index
-		this.isDummyBefore = isDummyBefore
-	}
-	
-	advance() {
-		if (this.isDummyBefore) {
-			this.isDummyBefore = false
-		} else {
-			this.isDummyBefore = true
-			++this.index
-		}
-	}
-}
 
 export class DateTimeWindowsIterator {
 	constructor({timeWindows, cal}) {
@@ -34,7 +16,7 @@ export class DateTimeWindowsIterator {
 		this._index = null
 		this._lastWindowUntilForever = null
 		if (this._timeWindows.length > 0) {
-			this._index = this._findInsertionIndex(this._timeWindows, cal.valueOf())
+			this._index = new DateTimeWindowsInserter().findInsertionIndex(this._timeWindows, cal.valueOf(), this._tz)
 			this._lastWindowUntilForever = !timeWindows[this._timeWindows.length-1].end
 		} else {
 			this._index = new Index(0, true)
@@ -47,48 +29,11 @@ export class DateTimeWindowsIterator {
 	 * @param date   availability.Date
 	 * @return Long
 	 */
-	_getTime(date) {
+	_getTime(date, tz) {
 		if (!date) {
             return null
 		}
-		return moment.tz([date.year, date.month - 1, date.day, date.hour, date.minute], this._tz).valueOf()
-	}
-	
-    _strictlyBefore(window1EndTs, window2StartTs) {
-        if ((window1EndTs === null) || (window2StartTs === null)) {
-            return false
-        }
-        return (window1EndTs <= window2StartTs)
-    }
-	
-	_compare(timestamp, timeWindow) {
-		if (this._strictlyBefore(timestamp + 1000, this._getTime(timeWindow.start))) {
-			return -1
-		} else if (this._strictlyBefore(this._getTime(timeWindow.end), timestamp)) {
-			return 1
-		} else {
-			return 0
-		}
-	}
-	
-	/**
-	 * @param timeWindows   List<DateTimeWindow>
-	 * @param timestamp     Long
-	 * @return Index
-	 */
-	_findInsertionIndex(timeWindows, timestamp) {
-		// TODO: use binary search
-		for (let i = 0, l = timeWindows.length; i < l; ++i) {
-			let timeWindow = timeWindows[i]
-			
-			let c = this._compare(timestamp, timeWindow)
-			if (c < 0) {
-				return new Index(i, true)
-			} if (c === 0) {
-				return new Index(i, false)
-			}
-		}
-		return new Index(timeWindows.length, true)
+		return moment.tz([date.year, date.month - 1, date.day, date.hour, date.minute], tz).valueOf()
 	}
 	
 	/** @return Boolean */
@@ -114,14 +59,14 @@ export class DateTimeWindowsIterator {
 			if (!this._index.isDummyBefore) {
 				result = {
 					status: (nextTimeWindow.available ? Status.STATUS_AVAILABLE : Status.STATUS_UNAVAILABLE),
-					until: this._getTime(nextTimeWindow.end),
+					until: this._getTime(nextTimeWindow.end, this._tz),
 					reason: nextTimeWindow.reason,
 					comment: nextTimeWindow.comment
 				}
 			} else {
 				result = {
 					status: Status.STATUS_UNKNOWN,
-					until: this._getTime(nextTimeWindow.start)
+					until: this._getTime(nextTimeWindow.start, this._tz)
 				}
 			}
 		}
