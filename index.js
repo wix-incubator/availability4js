6b12581cc9bc36254f93fddbfdde0ec960172185
dleshem <dleshem@gmail.com>
2016-05-12 19:54:48 +0300

    No longer uses webpack, just babel.

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-import {AvailabilityIterator} from "./src/AvailabilityIterator.js"
-import {DisjunctiveAvailabilityIterator} from "./src/DisjunctiveAvailabilityIterator.js"
-import * as Status from "./src/Status.js"
-
-export {AvailabilityIterator as AvailabilityIterator}
-export {DisjunctiveAvailabilityIterator as DisjunctiveAvailabilityIterator}
-export {Status as Status}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+import {AvailabilityIterator} from "./AvailabilityIterator"
+import {DisjunctiveAvailabilityIterator} from "./DisjunctiveAvailabilityIterator"
+import * as Status from "./Status"
+
+export {AvailabilityIterator as AvailabilityIterator}
+export {DisjunctiveAvailabilityIterator as DisjunctiveAvailabilityIterator}
+export {Status as Status}
